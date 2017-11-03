81ee637b9e05b64f92b43a999667ad23fe264704
Gavin Loughridge <gavinlough@gmail.com>
2017-11-03 15:23:21 -0500

    added solutions and bonus

--- index.js
+++ index.js
@@ -2,4 +2,37 @@
   'use strict';
 
   // YOUR CODE HERE
+  controls.addEventListener('click', lightSwitch);
+  stopButton.addEventListener('mouseenter', logEnter);
+  slowButton.addEventListener('mouseenter', logEnter);
+  goButton.addEventListener('mouseenter', logEnter);
+  stopButton.addEventListener('mouseleave', logExit);
+  slowButton.addEventListener('mouseleave', logExit);
+  goButton.addEventListener('mouseleave', logExit);
+
+//event.target.textContent.toLowerCase()
+  function lightSwitch() {
+    switch(event.target) {
+      case stopButton:
+        stopLight.classList.toggle('stop');
+        stopLight.classList.contains('stop')? console.log("stop bulb on"): console.log("stop bulb off");
+        break;
+      case slowButton:
+        slowLight.classList.toggle('slow');
+        slowLight.classList.contains('slow')? console.log("slow bulb on"): console.log("slow bulb off");
+        break;
+      case goButton:
+        goLight.classList.toggle('go');
+        goLight.classList.contains('go')? console.log("go bulb on"): console.log("go bulb off");
+        break;
+    }
+  }
+
+  function logEnter() {
+    console.log(`Entered ${event.target.textContent} button`);
+  }
+
+  function logExit() {
+    console.log(`Left ${event.target.textContent} button`);
+  }
 })();
